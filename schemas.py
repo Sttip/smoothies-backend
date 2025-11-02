c070a234b73fbbbeae23db1be1ae51fc4758c2e8
Sttip <sttip5789@gmail.com>
2025-11-02 13:09:57 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,28 @@
-# Aquí definiremos los esquemas Pydantic (entrada/salida):
-# - class ProductOut(BaseModel): ...
-# - class OrderCreate(BaseModel): ...
-# - class OrderOut(BaseModel): ...
+from pydantic import BaseModel, EmailStr
+from typing import List
 
+# -------- Productos (salida) --------
+class ProductOut(BaseModel):
+    id: int
+    slug: str
+    name: str
+    description: str
+    price: int
+    color: str
+    stock: int
+    class Config:
+        from_attributes = True  # (orm_mode en Pydantic v1)
+
+# -------- Órdenes (entrada/salida) --------
+class OrderLineIn(BaseModel):
+    product_id: int
+    qty: int
+
+class OrderCreate(BaseModel):
+    email: EmailStr
+    items: List[OrderLineIn]
+
+class OrderOut(BaseModel):
+    id: int
+    email: EmailStr
+    total: int
